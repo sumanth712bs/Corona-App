71cbd939097183dc1647ecd585e13f26a82e079b
Sumanth Badadal <sumanth.badadal@scout24.com>
2020-07-23 20:37:07 +0200

    TRK-4: Add Country picker functionalities

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Cards, Chart, CountryPicker} from "./components";
 import styles from "./App.module.css";
 import {fetchData} from "./api";
+import coronaImage from './images/image.png';
 
 class App extends React.Component {
     state = {
@@ -11,20 +12,22 @@ class App extends React.Component {
 
     async componentDidMount() {
         const data = await fetchData();
-        this.setState({data})
+        this.setState({data: data})
     }
 
-    handleCountryChange  = async (country) => {
-        console.log(country);
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({data: fetchedData, country: country})
     }
 
     render() {
-        const {data} = this.state;
+        const {data, country} = this.state;
         return (
             <div className={styles.container}>
+                <img className={styles.image} alt="COVID-19" src={coronaImage}/>
                 <Cards data={data}/>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
-                <Chart/>
+                <Chart data={data} country={country}/>
             </div>
         );
     }
